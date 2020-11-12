cab7df1338ebdcee38eefea844fdca6edecfc3d3
ceyhuncakir <ceyhuncakir@live.nl>
2020-11-12 18:24:15 +0100

    Data folder aangemaakt
    Data folder aangemaakt met de volgende bestand er in :  steam.json
    
    - Ceyhun

--- sprint 1 - AI/main.py
+++ sprint 1 - AI/main.py
@@ -3,11 +3,12 @@ steam = {}
 
 
 def inladen():
-    with open('steam.json', 'r') as steamdata:
+    with open('./Data/steam.json', 'r') as steamdata:
         data = json.load(steamdata)
         counter = 0
         for item in data:
             steam.update({counter: data[counter]})
             counter += 1
+
 
 
