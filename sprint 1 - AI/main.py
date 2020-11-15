0ce6d69229d00a3fb9ce225e98943f78ce8e0211
GMKenny <kenny.vandenberg@student.hu.nl>
2020-11-15 14:26:22 +0100

    Aanpassing
    Toen we er laats naar keken zagen we dat item niet werd gebruikt.  data[counter] kon verandert worden naar item.

--- sprint 1 - AI/main.py
+++ sprint 1 - AI/main.py
@@ -7,8 +7,10 @@ def inladen():
         data = json.load(steamdata)
         counter = 0
         for item in data:
-            steam.update({counter: data[counter]})
+            steam.update({counter: item})  # data[counter] verandert naar item. het doet de zelfde handeling.
             counter += 1
 
 #testing
-
+inladen()
+print(steam[0])
+print(steam[1])
